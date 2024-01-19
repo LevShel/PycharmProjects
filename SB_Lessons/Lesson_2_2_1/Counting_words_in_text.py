c159a145efb205dfcca91f1b8c93eab519c4c3e3
ШелЛ <lev.shel@gmail.com>
2024-01-20 00:26:18 +0200

    add 1st version

---
+++ SB_Lessons/Lesson_2_2_1/Counting_words_in_text.py
@@ -0,0 +1,22 @@
+words_list = []
+counts = [0, 0 ,0]
+
+num_words = int(input('Enter nums of searchig words: '))
+
+for i in range(num_words):
+    print('Enter ', i+1, ' searching word: ', end='')
+    word = input()
+    words_list.append(word)
+
+print('Enter text by words. Dot will end text.')
+text = input('>: ')
+while text != '.':
+    for i in range(num_words):
+        if words_list[i] == text:
+            counts[i] +=1
+    text = input('>: ')
+
+print('\n Counting words:')
+print('There are ', end='')
+for i in range(num_words):
+    print(counts[i], ' words "', words_list[i], '", ', end='')
---
+++ SB_Lessons/Lesson_2_2_1/Replace_symbol.py
@@ -0,0 +1,33 @@
+Word = input('Enter word: ')
+NumReplaceSymbol = int(input('Number of replaced symbol: '))
+NewSymbol = input('Enter new symbol: ')
+
+# ВАРИАНТ 1
+#
+# NewWord = ''
+#
+# Count = 0
+#
+# for Symbol in Word:
+#     Count += 1
+#     if Count != NumReplaceSymbol:
+#         NewWord += Symbol
+#     else:
+#         NewWord += NewSymbol
+#
+# print('New word is: ', NewWord)
+
+# ВАРИАНТ 2
+#
+# SymList = []
+#
+# for Sym in Word:
+#     SymList.append(Sym)
+# SymList[NumReplaceSymbol-1] = NewSymbol
+
+# ВАРИАНТ 3
+SymList = list(Word)
+SymList[NumReplaceSymbol-1] = NewSymbol
+
+for i in SymList:
+    print(i, end='')
