c99e724962319ace5ee030537f2a931512c8f45b
ШелЛ <lev.shel@gmail.com>
2024-01-20 22:08:12 +0200

    add 1st version

--- SB_Lessons/Lesson_2_2_1/Counting_words_in_text.py
+++ SB_Lessons/Lesson_2_2_1/Counting_words_in_text.py
@@ -1,22 +1,44 @@
-words_list = []
-counts = [0, 0 ,0]
+# words_list = []
+# counts = [0, 0 ,0]
+#
+# num_words = int(input('Enter nums of searchig words: '))
+#
+# for i in range(num_words):
+#     print('Enter ', i+1, ' searching word: ', end='')
+#     word = input()
+#     words_list.append(word)
+#
+# print('Enter text by words. Dot will end text.')
+# text = input('>: ')
+# while text != '.':
+#     for i in range(num_words):
+#         if words_list[i] == text:
+#             counts[i] +=1
+#     text = input('>: ')
+#
+# print('\n Counting words:')
+# print('There are ', end='')
+# for i in range(num_words):
+#     print(counts[i], ' words "', words_list[i], '", ', end='')
+
+words_list = [['',0],['',0],['',0]]
 
 num_words = int(input('Enter nums of searchig words: '))
 
 for i in range(num_words):
     print('Enter ', i+1, ' searching word: ', end='')
     word = input()
-    words_list.append(word)
+    words_list[i][0] = word
 
 print('Enter text by words. Dot will end text.')
 text = input('>: ')
 while text != '.':
     for i in range(num_words):
-        if words_list[i] == text:
-            counts[i] +=1
+        if words_list[i][0] == text:
+            words_list[i][1] +=1
     text = input('>: ')
 
 print('\n Counting words:')
 print('There are ', end='')
 for i in range(num_words):
-    print(counts[i], ' words "', words_list[i], '", ', end='')
+    print(words_list[i][1], ' words "', words_list[i][0], '", ', end='')
