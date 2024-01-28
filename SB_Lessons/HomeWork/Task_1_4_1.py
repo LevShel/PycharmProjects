8050f468f7d234a00738dcc7fcaaa552e3c80e0d
ШелЛ <lev.shel@gmail.com>
2024-01-28 17:44:25 +0200

    add new version

---
+++ SB_Lessons/HomeWork/Task_1_4_1.py
@@ -0,0 +1,41 @@
+# Описание
+# Практика
+#
+# Цель задания
+# Найти ошибки в работе с print.
+#
+# Что нужно сделать
+# Найдите и исправьте ошибки в коде.Для этого скопируйте код в repl.it, посмотрите, какие ошибки там есть, и исправьте их.
+# Убедитесь, что программа работает.
+#
+# 1.
+# Print('Hello World')
+#
+# 2.
+# print 'Hello World'
+#
+# 3.
+# print('Hello World")
+#
+# 4.
+# print(Hello)
+#
+# 5.
+# print('Привет', ')
+# print('Меня зовут Вася'')
+# print 'и я умнее всех')
+
+# 1. Print('Hello World')
+print('Hello World')
+# 2. print 'Hello World'
+print('Hello World')
+# 3. print('Hello World")
+print('Hello World')
+# 4. print(Hello)
+print('Hello')
+# 5. print('Привет', ')
+# print('Меня зовут Вася'')
+# print 'и я умнее всех')
+print('Привет, ')
+print('Меня зовут Вася')
+print('и я умнее всех')
