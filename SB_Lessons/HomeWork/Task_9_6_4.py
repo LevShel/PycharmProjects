a309f48494c0520029850e757e0f0142ac2ae5eb
ШелЛ <lev.shel@gmail.com>
2024-02-03 01:24:37 +0200

    add new version

--- SB_Lessons/HomeWork/Task_9_6_4.py
+++ SB_Lessons/HomeWork/Task_9_6_4.py
@@ -20,3 +20,28 @@
 # [Оператор]: W
 # [Программа]: Марсоход находится на позиции 5, 20, введите команду:
 
+def rover_move(size_x, size_y, x, y, move):
+    # move = input(f'The rover is at position [{x}, {y}]\n'
+    #              'enter the command: ').upper()
+    if move == 'W' and (0 <= y < size_y):
+        y += 1
+    elif move == 'A' and (0 < x <= size_x):
+        x -= 1
+    elif move == 'S' and (0 < y <= size_y):
+        y -= 1
+    elif move == 'D' and (0 <= x < size_x):
+        x += 1
+    return x, y
+
+
+size_x = 15
+size_y = 20
+
+x = 8
+y = 10
+
+while True:
+    move = input(f'The rover is at position [{x}, {y}]\n'
+                 'enter the command: ').upper()
+    x, y = rover_move(size_x, size_y, x, y, move)
+    
