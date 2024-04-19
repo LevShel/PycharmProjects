a8b781352ed56905f7fbc31d7a17994f763b1b83
ШелЛ <lev.shel@gmail.com>
2024-04-19 16:37:35 +0300

    add new version

--- SB_Lessons/HomeWork/Part_2/Module_15/Task_15_9_4.py
+++ SB_Lessons/HomeWork/Part_2/Module_15/Task_15_9_4.py
@@ -44,4 +44,55 @@
 # key2 : value2
 # key4 : value4
 
-# TODO
+from typing import Any
+
+
+class LRUCache:
+    def __init__(self, size: int) -> None:
+        self.size = size
+        self.memory = {}
+        self.keys = []
+
+    @property
+    def cache(self) -> dict:
+        return self.memory
+
+    @cache.setter
+    def cache(self, new_element: Any) -> None:
+        key, value = new_element
+        if len(self.memory) < self.size:
+            self.memory[key] = value
+            self.keys.append(key)
+        else:
+            old_key = self.keys[0]
+            self.memory.pop(old_key)
+            self.keys.remove(old_key)
+            self.memory[key] = value
+
+    def get(self, key: Any) -> str:
+        if key in self.memory.keys():
+            return f'{self.memory[key]}\n'
+
+    def print_cache(self) -> None:
+        cache_str = ''
+        print('\tLRU Cache:')
+        for key, value in self.memory.items():
+            cache_str += f'{key}: {value}\n'
+        return print(cache_str)
+
+
+# Создаём экземпляр класса LRU Cache с capacity = 3
+cache = LRUCache(3)
+# Добавляем элементы в кэш
+cache.cache = ("key1", "value1")
+cache.cache = ("key2", "value2")
+cache.cache = ("key3", "value3")
+# Выводим текущий кэш
+cache.print_cache()  # key1 : value1, key2 : value2, key3 : value3
+# Получаем значение по ключу
+print(cache.get("key2"))  # value2
+# Добавляем новый элемент, превышающий лимит capacity
+cache.cache = ("key4", "value4")
+cache.cache = ("key5", "value5")
+# Выводим обновлённый кэш
+cache.print_cache()  # key3 : value3, key4 : value4, key5 : value5
