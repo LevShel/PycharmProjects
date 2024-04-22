1d1a9c3b9ac4a3386cd9c92e27631021f1fe827c
ШелЛ <lev.shel@gmail.com>
2024-04-22 23:34:20 +0300

    add new version

--- SB_Lessons/Part_2/Lesson_2_18/requests.py
+++ SB_Lessons/Part_2/Lesson_2_18/requests.py
@@ -1,3 +1,6 @@
 import requests
+import json
 
-m = requests.get('https://www.google.com')
+my_req = requests.get('https://sqapi.dev/api/people/3/')
+data = json.loads(my_req.text)
+print(data['name'])
